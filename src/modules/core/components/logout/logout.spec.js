726a53cb577a677aff85c9cab2108a0e83f88d6f
Ernest Gaigulo <ernest@tangentsolutions.co.za>
2016-09-20 20:04:51 +0200

    logout component - initial branch upload

---
+++ src/modules/core/components/logout/logout.spec.js
@@ -0,0 +1,8 @@
+import 'angular-mocks';
+import controller from './logout.controller';
+
+describe('some test', () => {
+    it('should expect something', function() {
+        expect(controller).toBeDefined();
+    });
+});
